458e5644d9631109ac969dbb366a73da35766783
Tim Maupin <timmaupin@live.com>
2023-06-01 10:20:49 -0500

    create event-pool and hub js

--- hub.js
+++ hub.js
@@ -1,2 +1,15 @@
 'use strict';
 
+let eventEmitter = require('./event-pool');
+
+require('./vendor/index');
+require('./driver/index');
+
+eventEmitter.on('pickup', (payload) => logger('pickup', payload));
+eventEmitter.on('on the move', (payload) => logger('on the move', payload));
+eventEmitter.on('delivered', (payload) => logger('delivered', payload));
+
+function logger(event, payload){
+    const timestamp = new Date();
+    console.log('Event:', { event. timestamp, payload});
+}
